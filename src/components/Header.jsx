723dd6d467c98fb5a5927ef632522a28623b8362
Iryna-Bzdyr <57641315+Iryna-Bzdyr@users.noreply.github.com>
2020-03-28 18:24:27 +0200

    version 1.0.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://firebasestorage.googleapis.com/v0/b/roshen-bebec.appspot.com/o/icons%2Fcalendar.png?alt=media&token=9f4d0b79-9e68-4d9b-83e8-c2f4dbd9d947' alt=''></img>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='content-background'>
+
+            </div>
+            <div className='content-inner'>
+                <div>
+                    ava+description
+       </div>
+                <div>
+                    My post
+         <div>
+                        New post
+              <div>
+                            <div>Post 1</div>
+                            <div>Post 2</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
