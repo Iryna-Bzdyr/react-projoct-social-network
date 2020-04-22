adc8eda333ca608df3024b9455e12530a03501ff
Iryna-Bzdyr <57641315+Iryna-Bzdyr@users.noreply.github.com>
2020-04-23 01:13:59 +0300

    version 1.0.15

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,36 +3,33 @@ import s from './ProfileInfo.module.css'
 import PreLoader from "../../../common/PreLoader/PreLoader";
 
 
-
 const ProfileInfo = (props) => {
-    console.log(props)
-if (props.userData.length==0){
-   return <PreLoader/>
-}
-else {
-    return (
-        <div>
-            <div className={s.wrapper}>
-                <div className={s.background}>
-                    <img src='https://i.pinimg.com/originals/a0/6d/06/a06d06c67ecb1edfeb40c5fa2995f318.jpg'/>
-                    <div className={s.profile__info}>
-                        <img src={props.userData[0].photo}/>
-                        <div>
-                            <h4>
-                                {props.userData[0].fullName.firstName}   {props.userData[0].fullName.lastName}
-                            </h4>
-                        </div>
-                        <div>
-                            <span>{props.userData[0].location.city}</span>  <span>{props.userData[0].location.country}</span>
+    if (props.userData.length == 0) {
+        return <PreLoader/>
+    } else {
+        return (
+            <div>
+                <div className={s.wrapper}>
+                    <div className={s.background}>
+                        <img src='https://i.pinimg.com/originals/a0/6d/06/a06d06c67ecb1edfeb40c5fa2995f318.jpg'/>
+                        <div className={s.profile__info}>
+                            <img src={props.userData[0].photo}/>
+                            <div>
+                                <h4>
+                                    {props.userData[0].fullName.firstName} {props.userData[0].fullName.lastName}
+                                </h4>
+                            </div>
+                            <div>
+                                <span>{props.userData[0].location.city}</span>
+                                <span>{props.userData[0].location.country}</span>
+                            </div>
                         </div>
                     </div>
+
                 </div>
-
             </div>
-        </div>
-    )
-}
-
+        )
+    }
 }
 
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,49 +2,55 @@ import React from "react";
 import s from './Users.module.css'
 import {NavLink} from "react-router-dom";
 
-const Users = (props)=>{
-    let pagesCount = Math.ceil(props.totalUsersCount/ props.pageSize)
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
     let pagesArray = []
-    for (let i=1; i<=pagesCount; i++){
+    for (let i = 1; i <= pagesCount; i++) {
         pagesArray.push(i)
     }
-    return(
+    return (
         <div>
             <div className={s.pages__block}>
                 {pagesArray.map((page, index) =>
-                    <button onClick={()=>props.onPageChange(page, index)} className={props.currentPage===page?s.activePage:''}
+                    <button onClick={() => props.onPageChange(page, index)}
+                            className={props.currentPage === page ? s.activePage : ''}
                     >{page}</button>)
                 }
             </div>
             {
-                props.users.map(u => <div className={s.user__card}>
-                    <div className={s.user__avatar}>
-                        <NavLink to={`/profile/${u.id}`}>
-                            <img src={u.photo}/>
-                        </NavLink>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => {
-                                    props.unFollow(u.id)
-                                }} className={s.unfollow__btn}>UNFOLLOW</button>
-                                : <button onClick={() => {
-                                    props.follow(u.id)
-                                }} className={s.follow__btn}>FOLLOW</button>
-                            }
+                props.users.map(u => <NavLink to={`/profile/${u.id}`}>
+
+                        <div className={s.user__card}>
+                            <div className={s.user__avatar}>
+
+                                <img src={u.photo}/>
+
+                                <div>
+                                    {u.followed
+                                        ? <button onClick={() => {
+                                            props.unFollow(u.id)
+                                        }} className={s.unfollow__btn}>UNFOLLOW</button>
+                                        : <button onClick={() => {
+                                            props.follow(u.id)
+                                        }} className={s.follow__btn}>FOLLOW</button>
+                                    }
+                                </div>
+                            </div>
+                            <div className={s.user__description}>
+                                <div className={s.name}>
+                                    <h3>{u.fullName.firstName} {u.fullName.lastName}</h3>
+                                </div>
+                                <div className={s.locotion}>
+                                    <p>{u.location.city}, {u.location.country}</p>
+                                </div>
+                                <div className={s.status}>
+                                    <p>{u.status}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className={s.user__description}>
-                        <div className={s.name}>
-                            <h3>{u.fullName.firstName} {u.fullName.lastName}</h3>
-                        </div>
-                        <div className={s.locotion}>
-                            <p>{u.location.city}, {u.location.country}</p>
-                        </div>
-                        <div className={s.status}>
-                            <p>{u.status}</p>
-                        </div>
-                    </div>
-                </div>)
+                    </NavLink>
+                )
+
             }
         </div>
     )
