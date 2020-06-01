edda17da37fbd3243be8a94652db77383bf6011f
Iryna-Bzdyr <57641315+Iryna-Bzdyr@users.noreply.github.com>
2020-06-01 22:32:27 +0300

    version 20

--- src/Redux/Reducer/location-reducer.js
+++ src/Redux/Reducer/location-reducer.js
@@ -1,4 +1,4 @@
-import  {countryAPI} from "../../firebase";
+import {countryAPI} from "../../firebase";
 import {toggleIsFetchingAC} from "./user-reducer";
 
 const setCountries = 'SET-COUNTRIES'
@@ -6,41 +6,40 @@ let initialState = {
     countries: []
 }
 
-let locationReducer = (state=initialState, action)=>{
+let locationReducer = (state = initialState, action) => {
     switch (action.type) {
         case setCountries:
             return {
                 ...state,
-                countries:[action.countries]
+                countries: [action.countries]
             }
         default:
             return state
     }
 }
 
-const setCountriesAC =  (countries) => ({type:setCountries, countries:countries})
+const setCountriesAC = (countries) => ({type: setCountries, countries: countries})
 
-export const setCountriesThunk = ()=>(dispatch)=>{
+export const setCountriesThunk = () => (dispatch) => {
     let countries = []
     let newCountries = []
-    countryAPI.on('value', (snap)=> {
+    countryAPI.on('value', (snap) => {
         let countriesArray = []
-        snap.forEach(u=>{
+        snap.forEach(u => {
             countriesArray.push(u.val())
         })
-        newCountries = countriesArray
-       countriesArray.forEach(el=>{
-
-               countries.push({country:el.country})
-
-       })
+        countriesArray.map(el => {
+            if(!countries.map(c=>(c.country)).includes(el.country)){
+                countries.push({country: el.country})
+            }
+            countries.sort((a,b)=>( a.country > b.country) ? 1 : ((b.country > a.country) ? -1 : 0))
+        })
         dispatch(setCountriesAC(countries))
 
     });
-    if(countries.length>0){
+    if (countries.length > 0) {
         dispatch(toggleIsFetchingAC(false))
-    }
-    else{
+    } else {
         dispatch(toggleIsFetchingAC(true))
     }
 }
