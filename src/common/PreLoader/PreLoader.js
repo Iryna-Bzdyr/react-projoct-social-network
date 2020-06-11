bff9a1d3a8fa0cbbf8314555c621191198201d26
Iryna-Bzdyr <57641315+Iryna-Bzdyr@users.noreply.github.com>
2020-06-12 00:46:42 +0300

    commit 35

--- src/common/PreLoader/PreLoader.js
+++ src/common/PreLoader/PreLoader.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Loader from "react-loader-spinner";
+import s from './PreLoader.module.css'
 
 const PreLoader = (props)=>{
     return (
-        <div className='spinner'>
+        <div className={s.spinner}>
             <Loader
                 type="Bars" color="#fc03f0" height={100} width={100} secondaryColor="#2803fc"
             />
