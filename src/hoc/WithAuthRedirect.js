ca251a38577f32b469f071e4e8496da7b8f72bcc
Iryna-Bzdyr <57641315+Iryna-Bzdyr@users.noreply.github.com>
2020-06-25 00:31:47 +0300

    commit 35

--- src/hoc/WithAuthRedirect.js
+++ src/hoc/WithAuthRedirect.js
@@ -1,14 +1,20 @@
 import React from "react";
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
+import { push } from 'react-router-redux';
 
 let mapStateToProps = (state) => {
     return {
         resultCode:state.auth.resultCode,
     }
 }
+
+// const mapDispatchToProps = dispatch => bindActionCreators({
+//     redirect: () => push('/signin')
+// }, dispatch)
+
 export const withAuthRedirect = (Component)=>{
-    
+
     class RedirectComponent extends React.Component{
         render() {
             if (!this.props.resultCode){
@@ -23,3 +29,5 @@ export const withAuthRedirect = (Component)=>{
     const ConnectRedirectComponent = connect(mapStateToProps)(RedirectComponent)
     return ConnectRedirectComponent
 }
+
+
